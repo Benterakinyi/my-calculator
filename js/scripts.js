b35b409a634ed1fec444bda5fca42eda22ef5a3f
Benterakinyi <benterakinyi80@gmail.com>
2019-08-19 15:47:04 +0300

    add validation of day

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var maleNames = [Kwasi,Kwadwo,Kwabena,Kwaku,Yaw,Kofi,Kwame];
+var femaleNames = [Akosua,Adwoa,Abenaa,Akua,Yaa,Afua,Ama];
+
+validation(akanNames) {
+    var day = document.getElementById("day").Value; 
+}
+if(day<=0 || day >31){
+    alert("invalid date");
+}else if(day ==="" || day ==null){
+    alert("empty date");
+}
+
+validation(akanNames) {
+    var month = document.getElementById("month").value;
+}
+if (month <=0 // >12){
+      alert("invalid number");
+}else if(month ==="" || month == null){
+alert ("empty number");
+}
+validation(akanNames) 
+var year = document.getElementById("year");
