ea6b1f815ff65bc073abc3ecf145b845ce433715
Benterakinyi <benterakinyi80@gmail.com>
2019-08-21 15:52:10 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,10 @@ function submition(){
      var year=document.getElementById("year").value;
      var year=parseInt(year);
      var century=parseInt(year-1)/100+1;
-    }
- /* the formula */
- var result=parseInt( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day) % 7 ;
+
+
+      /* the formula */
+ var result=parseInt(((century /4) -2*century-1) + ((5*year/4)) + ((26*(month+1)/10)) + day) % 7 ;
  var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
@@ -90,6 +91,8 @@ else
 alert("input data please");
 }
 
+
+    }
 
 
 
